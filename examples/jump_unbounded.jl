516b42bb8c659b7a32cf64d77cd64bf096e9f08f
yeesian <ngyeesian@gmail.com>
2014-12-20 14:45:25 -0500

    remove jump test
    to avoid potential licensing issues

--- examples/jump_unbounded.jl
+++
@@ -1,7 +0,0 @@
-using JuMP, KNITRO, Base.Test
-m = Model(solver=KnitroSolver(objrange=1e16))
-@defVar(m, x >= 0)
-@setNLObjective(m, Max, x)
-@addNLConstraint(m, x >= 5)
-status = solve(m)
-@test status == :Unbounded
