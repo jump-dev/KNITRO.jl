47ea3a969d25b8f0c964b8da0f18c43b4bc0a910
François Pacaud <frapac@users.noreply.github.com>
2018-11-21 23:04:41 +0100

    Fix problems in MathProgBase tests (#90)
    Fix mathprogbase tests
    Use tighter opttol instead of opttol_abs for Rosenbrock test

--- test/mathprogtest.jl
+++ test/mathprogtest.jl
@@ -1,5 +1,9 @@
-include(joinpath(Pkg.dir("MathProgBase"),"test","nlp.jl"))
+# Test KNITRO with MathProgBase unit-tests
+#
+using MathProgBase, KNITRO
+
+include(joinpath(dirname(pathof(MathProgBase)),"..", "test", "nlp.jl"))
 convexnlptest(KnitroSolver())
-rosenbrocktest(KnitroSolver())
+rosenbrocktest(KnitroSolver(opttol=1e-7))
 nlptest(KnitroSolver())
 nlptest_nohessian(KnitroSolver())
