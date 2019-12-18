34549a679e560dd1b0815b19079ea1c3af6cfcfe
fpacaud <francois.pacaud@artelys.com>
2019-12-18 09:19:53 +0100

    hotfix: change option file used in tests

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -57,7 +57,7 @@ const BRIDGED = MOIB.full_bridge_optimizer(OPTIMIZER, Float64)
     @testset "MOI.RawAttribute" begin
         # Test special RawAttributes
         optimizer = KNITRO.Optimizer()
-        option_file = joinpath(dirname(pathof(KNITRO)),"..", "examples", "test_knitro.opt")
+        option_file = joinpath(dirname(pathof(KNITRO)),"..", "examples", "knitro.opt")
         MOI.set(optimizer, MOI.RawParameter("option_file"), option_file)
         tuner_file = joinpath(dirname(pathof(KNITRO)),"..", "examples", "tuner-fixed.opt")
         MOI.set(optimizer, MOI.RawParameter("tuner_file"), tuner_file)
