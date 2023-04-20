204efebbd8229209798295147b8bfe1d6f7a7c0a
odow <o.dowson@gmail.com>
2023-04-20 20:27:12 +1200

    Update

--- src/KNITRO.jl
+++ src/KNITRO.jl
@@ -9,41 +9,40 @@ import Libdl
 import SparseArrays
 
 const _DEPS_FILE = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
-if isfile(_DEPS_FILE)
+
+if !isfile(_DEPS_FILE)
     include(_DEPS_FILE)
 else
     error("KNITRO.jl not properly installed. Please run `] build KNITRO`")
 end
 
+const IS_KNITRO_LOADED[] = false
+const KNITRO_VERSION[] = VersionNumber(0, 0, 0) # Fake a version for AutoMerge
+
+has_knitro() = IS_KNITRO_LOADED[]
+knitro_version() = KNITRO_VERSION[]
+
 function __init__()
     @show libknitro
     @show readdir(dirname(_DEPS_FILE))
     @show read(_DEPS_FILE, String)
+    IS_KNITRO_LOADED[] = endswith(libknitro, Libdl.dlext)
+    if IS_KNITRO_LOADED[]
+        len = 15
+        out = zeros(Cchar, len)
+        ccall((:KTR_get_release, libknitro), Any, (Cint, Ptr{Cchar}), len, out)
+        res = String(strip(String(convert(Vector{UInt8}, out)), '\0'))
+        KNITRO_VERSION[] = VersionNumber(split(res, " ")[2])
+    end
+    if KNITRO_VERSION[] != v"0.0.0" && KNITRO_VERSION[] < v"11.0"
+        error(
+            "You have installed version $(KNITRO_VERSION[]) of Artelys " *
+            "Knitro, which is not supported by KNITRO.jl. We require a " *
+            "Knitro version greater than 11.0.",
+        )
+    end
     return
 end
-
-const IS_KNITRO_LOADED = endswith(libknitro, Libdl.dlext)
-
-const KNITRO_VERSION = if IS_KNITRO_LOADED
-    len = 15
-    out = zeros(Cchar, len)
-    ccall((:KTR_get_release, libknitro), Any, (Cint, Ptr{Cchar}), len, out)
-    res = String(strip(String(convert(Vector{UInt8}, out)), '\0'))
-    VersionNumber(split(res, " ")[2])
-else
-    VersionNumber(0, 0, 0) # Fake a version for AutoMerge
-end
-
-if KNITRO_VERSION != v"0.0.0" && KNITRO_VERSION < v"11.0"
-    error(
-        "You have installed version $KNITRO_VERSION of Artelys Knitro, which " *
-        "is not supported by KNITRO.jl. We require a Knitro version greater " *
-        "than 11.0.",
-    )
-end
-
-has_knitro() = IS_KNITRO_LOADED
-knitro_version() = KNITRO_VERSION
 
 include("libknitro.jl")
 include("C_wrapper.jl")
