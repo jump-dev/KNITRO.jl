d0d16950e1b95ab9b8f9322d20581910a1b86b44
odow <o.dowson@gmail.com>
2023-11-03 15:31:43 +1300

    Fix NLPBlockDualStart and tidy MOI tests

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -6,9 +6,9 @@
 module TestMOIWrapper
 
 using Test
-using KNITRO
 
-const MOI = KNITRO.MOI
+import KNITRO
+import MathOptInterface as MOI
 
 function runtests()
     for name in names(@__MODULE__; all=true)
@@ -22,38 +22,22 @@ function runtests()
 end
 
 function test_MOI_Test_cached()
-    second_order_exclude = String[
-        "test_conic_GeometricMeanCone_VectorAffineFunction",
-        "test_conic_GeometricMeanCone_VectorAffineFunction_2",
-        "test_conic_GeometricMeanCone_VectorOfVariables",
-        "test_conic_GeometricMeanCone_VectorOfVariables_2",
-        "test_conic_RotatedSecondOrderCone_INFEASIBLE_2",
-        "test_conic_RotatedSecondOrderCone_VectorAffineFunction",
-        "test_conic_RotatedSecondOrderCone_VectorOfVariables",
-        "test_conic_RotatedSecondOrderCone_out_of_order",
-        "test_conic_SecondOrderCone_Nonpositives",
-        "test_conic_SecondOrderCone_Nonnegatives",
-        "test_conic_SecondOrderCone_VectorAffineFunction",
-        "test_conic_SecondOrderCone_VectorOfVariables",
-        "test_conic_SecondOrderCone_out_of_order",
-        "test_constraint_PrimalStart_DualStart_SecondOrderCone",
-    ]
-    model = MOI.Utilities.CachingOptimizer(
-        MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
-        MOI.instantiate(KNITRO.Optimizer; with_bridge_type=Float64),
+    model = MOI.instantiate(
+        KNITRO.Optimizer;
+        with_bridge_type=Float64,
+        with_cache_type=Float64,
     )
     MOI.set(model, MOI.Silent(), true)
     MOI.Test.runtests(
         model,
         MOI.Test.Config(
-            atol=1e-4,
-            rtol=1e-4,
+            atol=1e-3,
+            rtol=1e-3,
             optimal_status=MOI.LOCALLY_SOLVED,
             infeasible_status=MOI.LOCALLY_INFEASIBLE,
             exclude=Any[
                 MOI.ConstraintBasisStatus,
                 MOI.VariableBasisStatus,
-                MOI.DualObjectiveValue,
             ],
         );
         exclude=String[
@@ -67,24 +51,7 @@ function test_MOI_Test_cached()
             "test_solve_ObjectiveBound_MAX_SENSE_LP",
             # KNITRO doesn't support INFEASIBILITY_CERTIFICATE results.
             "test_solve_DualStatus_INFEASIBILITY_CERTIFICATE_",
-            second_order_exclude...,
         ],
-    )
-    MOI.Test.runtests(
-        model,
-        MOI.Test.Config(
-            atol=1e-3,
-            rtol=1e-3,
-            optimal_status=MOI.LOCALLY_SOLVED,
-            infeasible_status=MOI.LOCALLY_INFEASIBLE,
-            exclude=Any[
-                MOI.ConstraintBasisStatus,
-                MOI.VariableBasisStatus,
-                MOI.DualObjectiveValue,
-                MOI.ConstraintDual,
-            ],
-        );
-        include=second_order_exclude,
     )
     return
 end
