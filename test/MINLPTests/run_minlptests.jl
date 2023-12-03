9ca7a7bd00aaf84cd3a8a1ead12b9a67db591b98
Oscar Dowson <odow@users.noreply.github.com>
2023-12-04 11:58:02 +1300

    Update run_minlptests.jl

--- test/MINLPTests/run_minlptests.jl
+++ test/MINLPTests/run_minlptests.jl
@@ -12,17 +12,14 @@ using Test
     solver =
         JuMP.optimizer_with_attributes(KNITRO.Optimizer, "outlev" => 0, "opttol" => 1e-8)
     # 005_010 : knitro finds a slightly different solution
-    # 005_011 : fixed in upcoming MINLPTests@0.5.7
-    # 006_010 : fixed in upcoming MINLPTests@0.5.7
     # 008_010 : MINLPTests.jl#21
-    # 008_011 : MINLPTests.jl#21
-    MINLPTests.test_nlp(solver; exclude=["005_010", "005_011"])
-    MINLPTests.test_nlp_expr(solver; exclude=["005_010", "005_011", "008_010", "008_011"])
+    MINLPTests.test_nlp(solver; exclude=["005_010"])
+    MINLPTests.test_nlp_expr(solver; exclude=["005_010", "008_010"])
     # For 005_010, Knitro founds a different solution, close to those of MINLPTests.
     MINLPTests.nlp_005_010(solver, 1e-5, 1e-5, 1e-5)
     MINLPTests.nlp_expr_005_010(solver, 1e-5, 1e-5, 1e-5)
     MINLPTests.test_nlp_cvx(solver)
     MINLPTests.test_nlp_cvx_expr(solver)
-    MINLPTests.test_nlp_mi(solver, exclude=["005_011", "006_010"])
-    MINLPTests.test_nlp_mi_expr(solver, exclude=["005_011", "006_010"])
+    MINLPTests.test_nlp_mi(solver)
+    MINLPTests.test_nlp_mi_expr(solver)
 end
