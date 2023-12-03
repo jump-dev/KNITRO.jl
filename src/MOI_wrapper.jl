3ded9b2e251767cd8322ac383d3eed8522639cbb
odow <o.dowson@gmail.com>
2023-12-04 10:36:45 +1300

    Add support for MOI.UserDefinedFunction

--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -325,6 +325,26 @@ function MOI.set(model::Optimizer, attr::MOI.RawOptimizerAttribute, value)
     return
 end
 
+# MOI.UserDefinedFunction
+
+MOI.supports(model::Optimizer, ::MOI.UserDefinedFunction) = true
+
+function MOI.set(model::Optimizer, attr::MOI.UserDefinedFunction, args)
+    MOI.Nonlinear.register_operator(
+        model.nlp_model,
+        attr.name,
+        attr.arity,
+        args...,
+    )
+    return
+end
+
+### MOI.ListOfSupportedNonlinearOperators
+
+function MOI.get(model::Optimizer, attr::MOI.ListOfSupportedNonlinearOperators)
+    return MOI.get(model.nlp_model, attr)
+end
+
 MOI.get(model::Optimizer, ::MOI.NumberOfVariables) = length(model.variable_info)
 
 function MOI.get(model::Optimizer, ::MOI.ListOfVariableIndices)
