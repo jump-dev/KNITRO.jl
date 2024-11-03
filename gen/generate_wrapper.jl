57718999326726e6fe60653c464c8abeddcc2033
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-11-03 13:27:30 -0600

    Update generate_wrapper.jl

--- gen/generate_wrapper.jl
+++ gen/generate_wrapper.jl
@@ -18,3 +18,18 @@ build!(ctx)
 
 path = options["general"]["output_file_path"]
 format_file(path, YASStyle())
+
+str = read(path, String)
+str =
+"""
+# Copyright (c) 2016: Ng Yee Sian, Miles Lubin, other contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
+#!format:off
+""" * str *
+"""
+#!format:on
+"""
+write(path, str)
--- gen/prologue.jl
+++ gen/prologue.jl
@@ -1,6 +1,1 @@
-# Copyright (c) 2016: Ng Yee Sian, Miles Lubin, other contributors
-#
-# Use of this source code is governed by an MIT-style license that can be found
-# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
-
 const DBL_MAX = Float64(0x1.fffffffffffffp+1023)
