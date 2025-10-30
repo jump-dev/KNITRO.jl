e9f389b928967628ddb61578ea0ed22a6a046426
Oscar Dowson <o.dowson@gmail.com>
2025-10-31 10:05:58 +1300

    Re-enable some previously failing tests

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -73,16 +73,10 @@ function test_MOI_Test_cached()
         exclude=Union{String,Regex}[
             # Upstream bug because @odow is a muppet
             r"^test_basic_VectorOfVariables_VectorNonlinearOracle$",
-            # This is an upstream issue in MOI with bridges and support
-            # comparing VectorNonlinear and VectorQuadratic
-            r"^test_basic_VectorNonlinearFunction_GeometricMeanCone$",
             # Returns OTHER_ERROR, which is also reasonable.
             r"^test_conic_empty_matrix$",
-            # Uses the ZerosBridge and ConstraintDual
-            r"^test_conic_linear_VectorOfVariables_2$",
             # Returns ITERATION_LIMIT instead of DUAL_INFEASIBLE, which is okay.
             r"^test_conic_RotatedSecondOrderCone_INFEASIBLE$",
-            r"^test_linear_DUAL_INFEASIBLE$",
             # Incorrect ObjectiveBound with an LP, but that's understandable.
             r"^test_solve_ObjectiveBound_MAX_SENSE_LP$",
             # Cannot get ConstraintDualStart
