0db6a82f72475f1084c876418342f6f090dd2338
Oscar Dowson <o.dowson@gmail.com>
2025-12-18 08:54:10 +1300

    Update

--- gen/generate_wrapper.jl
+++ gen/generate_wrapper.jl
@@ -1,20 +1,18 @@
-using Clang
-using Clang.Generators
-using JuliaFormatter
+# Copyright (c) 2016: Ng Yee Sian, Miles Lubin, other contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
 
-cd(@__DIR__)
+using Clang: Generators
+import KNITRO_jll
 
-const HEADER_BASE = joinpath(ENV["KNITRODIR"], "include")
+const HEADER_BASE = joinpath(KNITRO_jll.artifact_dir, "knitro", "include")
 const KNITRO_H = joinpath(HEADER_BASE, "knitro.h")
 
-headers = [KNITRO_H]
-options = load_options(joinpath(@__DIR__, "generator.toml"))
-
-args = get_default_args()
-push!(args, "-I$HEADER_BASE")
-
-ctx = create_context(headers, args, options)
-build!(ctx)
-
-path = options["general"]["output_file_path"]
-format_file(path)
+Generators.build!(
+    Generators.create_context(
+        [KNITRO_H],
+        [Generators.get_default_args(); "-I$HEADER_BASE"],
+        Generators.load_options(joinpath(@__DIR__, "generator.toml")),
+    ),
+)
