bfafd18f0bd8867ca069b3224ea577d2ff70b41a
yemine0x01 <youssouf.emine@artelys.com>
2026-07-11 05:11:59 -0400

    Adding support for 16.0.0 (#397)

--- src/KNITRO.jl
+++ src/KNITRO.jl
@@ -46,11 +46,11 @@ end
 @static if isdefined(@__MODULE__, :libknitro)
     # deps.jl must define a local installation.
     let version = has_knitro() ? knitro_version() : v"15.0.0"
-        if !(v"13" <= version < v"16")
+        if !(v"13" <= version < v"17")
             error(
                 "You have installed version $version of Artelys Knitro, " *
                 "which is not supported by KNITRO.jl. We require a version " *
-                "in [13, 16)",
+                "in [13, 17)",
             )
         end
     end
